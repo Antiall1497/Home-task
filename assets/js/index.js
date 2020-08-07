80fa35e4b8922239c051fd3ae830809b1f32db6a
antiall1497 <antiall1497@gmail.com>
2020-08-07 15:42:17 +0300

    home task

---
+++ assets/js/index.js
@@ -0,0 +1,219 @@
+`use strict`;
+
+// let name = prompt("Enter your name");
+
+// console.log(`Your name is ${name}`);
+
+// let colorRed = "#f00";
+
+// console.log(`My favorite color is ${colorRed}`);
+
+// let check = false;
+
+// let undenfiendVariable;
+
+// console.log (typeof check);
+
+// console.log (typeof undenfiendVariable);
+
+// let nullValute = null;
+
+// console.log(typeof nullValute);
+
+// let inputOne = prompt("enter your num");
+// let inputTwo = prompt("enter your num2");
+
+// inputOne = +inputOne;
+// inputTwo = +inputTwo;
+
+// let testValue = String(50);
+
+// console.log(testValue + "  strings");
+
+// let boolValue = Boolean(50);
+
+// console.log(boolValue);
+
+// let numberOne = Number(inputOne);
+// let numberTwo = Number(inputTwo);
+
+// let result = numberOne + numberTwo;
+
+// console.log(result);
+
+// let input = prompt(`enter a part of hour`);
+
+// +input;
+
+// if (input <= 15) {
+//   console.log(`first part of hour`);
+// } else if (input > 15 && input <= 30) {
+//   console.log(`second part of hour`);
+// } else if (input > 30 && input <= 45) {
+//   console.log(`third part of hour`);
+// } else if (input > 45 && input <= 60) {
+//   console.log(`fourth part of hour`);
+// }
+
+// --------------- ПРАКТИКА ------------------------
+
+// let num_1 = Number("c");
+// let num_2 = Number("d");
+
+// let result = num_1 * num_2;
+
+// console.log(result);
+
+// let num_3 = Number("c");
+// let num_4 = Number("d");
+
+// let result_2 = num_3 * num_4;
+
+// console.log(result_2);
+
+// let num_5 = Number("c");
+// let num_6 = Number("d");
+
+// let result_3 = num_5 * num_6;
+
+// console.log(result_3);
+
+// let text = "javascript";
+
+// console.log(typeof javascript);
+
+// let number = 11;
+
+// console.log(typeof number);
+
+// let enter = true;
+
+// console.log(typeof enter);
+
+// let number_2 = 100;
+
+// console.log(typeof number_2);
+
+// let num = 1;
+// let num2 = 11;
+
+// num += num2;
+// console.log(num);
+
+// num -= num2;
+// console.log(num);
+
+// num *= num2;
+// console.log(num);
+
+// num /= num2;
+// console.log(num);
+
+// num += 1;
+// console.log(num);
+
+// num -= 1;
+// console.log(num);
+
+// let input = prompt("Enter number");
+
+// console.log(input ** 2);
+
+// let inputOne = prompt("Enter number");
+// let inputTwo = prompt("Enter number");
+
+// let num1 = parseInt(inputOne);
+// let num2 = parseInt(inputTwo);
+
+// console.log((num1 + num2) / 2);
+
+// let seconds = prompt("Enter any minutes");
+
+// console.log(seconds * 60);
+
+// let userName = prompt("Enter your name");
+
+// let greeting = "Hello";
+
+// let name = "userName";
+
+// console.log(greeting + ` ` + userName);
+
+// let input = prompt(`enter a part of hour`);
+
+// +input;
+
+// let input = +prompt("Enter number");
+
+// input = +input;
+
+// if (input === 10) {
+//   console.log(`Верно`);
+// } else if (input > 10 || input < 10) {
+//   console.log(`Неверно`);
+// } else {
+//   console.log(`Enter a valid number`);
+// }
+
+// let input = +prompt("Enter true or false");
+
+// input = +input;
+
+// if (input === true) {
+//   console.log(true);
+// } else if (input !== false) {
+//   console.log(false);
+// }
+
+// let buy = +prompt("Enter sum of purchases");
+
+// let sale = 3;
+
+// let sale2 = 5;
+
+// buy = +buy;
+
+// if (buy > 500 % sale) {
+//   console.log(buy % sale);
+// } else if (buy > 800 % sale2) {
+//   console.log(buy % sale2);
+// }
+
+// let iteration = 0;
+
+// while (iteration <= 11) {
+//   console.log(iteration);
+
+//   iteration++;
+// }
+
+// let iter = 0;
+
+// do {
+//   console.log("Wow");
+// } while (iter === undefined);
+
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i  );
+//     continue;
+//   break;
+//   }
+// }
+
+// let num;
+
+// do {
+//   num = prompt("Введите ваш возраст, вы должны быть старше 17");
+// } while (num < 17 && num);
+
+// let iteration = 0;
+
+// do {
+//   (iteration > 10 && iteration < 50) 
+//     if(i % 5 === 0) {
+//       console.log(i);
+//       continue;
+//     }
+
+
